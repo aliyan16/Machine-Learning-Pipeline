2ac10cadfe3dcc2e3b327d2d7411fb7ae6414cba
aliyan16 <aliyanm12376@gmail.com>
2025-04-04 11:51:39 +0500

    added dvclive to track the model evaluation results

--- src/modelEvaluation.py
+++ src/modelEvaluation.py
@@ -5,6 +5,9 @@ import pickle
 import json
 from sklearn.metrics import accuracy_score,precision_score,recall_score,roc_auc_score
 import logging
+import dvclive
+import yaml
+from dvclive import Live
 
 
 #logs name directory created
@@ -30,6 +33,18 @@ fileHandler.setFormatter(formatter)
 #logger object set
 logger.addHandler(consoleHandler)
 logger.addHandler(fileHandler)
+
+def loadParams(paramsPath:str)->dict:
+    '''load the parameters from Yaml file'''
+    try:
+        with open(paramsPath,'r') as file:
+            params=yaml.safe_load(file)
+            logger.debug('Parameters loaded for DVClive')
+            return params
+    except Exception as e:
+        logger.error('Unexpected error',e)
+        raise
+
 
 def loadModel(filePath:str):
     '''loads the model'''
@@ -86,11 +101,19 @@ def saveMetrices(metrices:dict,filePath:str)->None:
 
 def main():
     try:
+        params=loadParams(paramsPath='params.yaml')
         clf=loadModel('./models/model.pkl')
         testData=loadData('./data/processed/testTfid.csv')
         xtest=testData.iloc[:,:-1].values
         ytest=testData.iloc[:,-1].values
         metrices=EvaluateModel(clf,xtest,ytest)
+        #initiating dvclive code
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric('accuracy',metrices['accuracy'])
+            live.log_metric('precision',metrices['precision'])
+            live.log_metric('recall',metrices['recall'])
+            live.log_metric('auc',metrices['auc'])
+            live.log_params(params)
         saveMetrices(metrices,'reports/metrics.json')
     except Exception as e:
         logger.error(e)
